b9da0d285b2db8c2e03f05ca8a2a0dd1fa625f65
Peter N Jenkin <peternjenkin@gmail.com>
2019-09-02 14:17:04 +0100

    4-37 package.json and index.js to require and configure express to say hello there to request to root on 8080

---
+++ index.js
@@ -0,0 +1,17 @@
+// Set the server to listen on 8080 and define the server's response as a 'hello' string 
+// (For use in docker demo section 4 https://www.udemy.com/docker-and-kubernetes-the-complete-guide/learn/lecture/11436946#questions)
+
+const express = require('express');     // require express library
+
+const app = express();
+
+// Send this string as a response to root URL request
+app.get('/', (request, response) => {
+    response.send('Hello there');
+});
+
+// Listen on port 8080
+app.listen(8080, () =>
+{
+    console.log('Listening on port 8080');
+});
